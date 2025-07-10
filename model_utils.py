3843183d5f3a49bbf9773ab57a0b0a1ff3875e07
Jeffrey Haines <jeffhaines@users.noreply.huggingface.co>
2025-07-10 04:22:04 +0000

    Update model_utils.py

--- model_utils.py
+++ model_utils.py
@@ -103,6 +103,7 @@ def make_llm(tokenizer, model, temperature=0, token_limit=-1):
     return llm
 
 
+@GPU
 def code(file_input, n_codes=-1, temperature=0, user_prompt='', use_example=False, session_runs=[], token_limit=-1, chunk_size=1024, batch_size=1, model=model, tokenizer=tokenizer):
     print("DEBUG: Inside code tokenizer is", tokenizer)
     print("DEBUG: Inside code tokenizer.eos_token_id is", getattr(tokenizer, "eos_token_id", "None"))
@@ -193,6 +194,7 @@ def code(file_input, n_codes=-1, temperature=0, user_prompt='', use_example=Fals
     yield highlighted_html, code_dict, session_runs, coding_status, run_selector_update, run_selector_update, codes
 
 
+@GPU
 def cluster(full_text, code_dict, max_themes, temperature, use_example, session_runs, token_limit, chunk_size, model=model, tokenizer=tokenizer):
     # needs to run the cluster_chain on the code_dict, return a theme_dict, and visualize the clusters
     if session_runs is None:
@@ -281,6 +283,7 @@ def cluster(full_text, code_dict, max_themes, temperature, use_example, session_
     yield theme_dict, theme_network_html, html_highlighted_by_theme, session_runs, theme_status, run_selector_update, run_selector_update
 
 
+@GPU
 def summarize(theme_dict, code_dict, text, temperature, use_example, session_runs, token_limit, chunk_size, model=model, tokenizer=tokenizer):
     # needs to run the summary_chain on theme_dict, then combine all dictionaries and return a table of the combined_dict
     if session_runs is None:
